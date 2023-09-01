46506779961a558d03cb7149f34a20b23b2d747f
laszloscheers <laszloscheers.gmail.com>
2023-09-01 17:29:49 +0200

    Added original files without sensitive data

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// API info for the Football Data API -  use a HTTP header named "X-Auth-Token" with the token as the value.
+export const footballApi = {
+    link: "https://api.football-data.org/v2/",
+    token: process.env.REACT_APP_API_KEY_1
+};
